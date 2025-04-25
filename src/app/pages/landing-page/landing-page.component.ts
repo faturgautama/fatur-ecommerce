9c82314ccdcfef816fc70b289b6cff877db7280d
Fatur Gautama <fatur@paycombat.com>
2025-04-25 09:04:43 +0700

    feat: add ui produk terbaru dan produk tersedia

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LndBaseComponent } from "../../components/layout/landing/lnd-base/lnd-base.component";
+import { BehaviorSubject, Subject } from 'rxjs';
+import { CardProductComponent } from '../../components/card/card-product/card-product.component';
+import { ProductModel } from '../../model/product/product.model';
 import { CarouselModule } from 'primeng/carousel';
-import { Subject } from 'rxjs';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     imports: [
+        ButtonModule,
+        CarouselModule,
         LndBaseComponent,
+        CardProductComponent,
     ],
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.scss'
@@ -23,6 +29,13 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     ];
 
     ActiveBannersIndex = 0;
+
+    Product$ = new BehaviorSubject<ProductModel.IProduct[]>([
+        { id: '1', nama: 'Euodia 1', harga: 5000000, gambar_produk: '', created_at: new Date(), created_by: 'Admin', status: true },
+        { id: '2', nama: 'Euodia 2', harga: 5000000, gambar_produk: '', created_at: new Date(), created_by: 'Admin', status: true },
+        { id: '3', nama: 'Euodia 3', harga: 5000000, gambar_produk: '', created_at: new Date(), created_by: 'Admin', status: true },
+        { id: '4', nama: 'Euodia 4', harga: 5000000, gambar_produk: '', created_at: new Date(), created_by: 'Admin', status: true },
+    ]);
 
     constructor() { }
 
